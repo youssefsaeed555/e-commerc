cb5dca825210c2bb221f6794c6490739bd9c7c1a
yossefsaid555 <yossefsaid555@gmail.com>
2022-09-15 14:32:15 +0200

    prousct schema

---
+++ models/proudct.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose')
+
+const proudct = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        maxLength: [30, 'too max proudct title'],
+        minLength: [5, 'too min proudct title'],
+        trim: true
+    },
+    slug: {
+        type: String,
+        lowerCase: true,
+        required: true
+    },
+    description: {
+        type: String,
+        minLength: [20, 'too min proudct description'],
+        require: [true, 'description of proudct required']
+    },
+    quantity:
+    {
+        type: Number,
+        require: [true, 'quantity of proudct required']
+    },
+    sold:
+    {
+        type: Number,
+        default: 0
+    },
+    category:
+    {
+        type: mongoose.Types.ObjectId,
+        ref: 'Category',
+        required: [true, 'product must belong to category'],
+    },
+    brand: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Brands',
+        required: [true, 'product must belong to brand'],
+    },
+    subCategory: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'subCategory',
+    }],
+    price: {
+        type: Number,
+        max: [10, 'too amx length price'],
+        required: [true, 'proudct price required'],
+        trim: true
+    },
+    priceAfterDiscount:
+    {
+        type: Number,
+    },
+    color: [String],
+    images: [String],
+    coverImage:
+    {
+        type: String,
+        reuired: [true, 'cover of proudct required']
+    },
+    averageRaiting:
+    {
+        type: Number,
+        min: [1, 'raiting must be above or equal 1.0'],
+        max: [5, 'raiting must be less than or equal 1.0'],
+        default: 0
+    },
+    countOfRaiting:
+    {
+        type: Number,
+        default: 0
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model('Proudcts', proudct)
